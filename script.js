7ab9eb0d6c1bed87e5fe37aa1f9e5f0949990707
Boncuk1905 <83425658+Boncuk1905@users.noreply.github.com>
2025-07-16 16:44:03 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('imageUpload').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+
+    if (file && file.type.startsWith('image/')) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('preview').src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    } else {
+        alert("Vælg venligst et gyldigt billede.");
+    }
+});
